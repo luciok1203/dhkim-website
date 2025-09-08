f304df6b6bde7cde6fd2f1b228c834eb76c9e854
luciok1203 <luciok1203@naver.com>
2025-09-08 20:13:40 +0900

    README, 상단 이동 floating 버튼 추가

---
+++ README.md
@@ -0,0 +1,5 @@
+# 와플스튜디오 frontend 세미나 1주차 과제 1
+
+# 나를 소개하는 웹사이트 만들기
+
+## [웹사이트 바로가기](https://dhkim-website.vercel.app/)
--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log("Hello World!");
+// 버튼 요소 가져오기
+const scrollTopBtn = document.getElementById("scrollTopBtn");
+
+// 스크롤 이벤트 감지 → 일정 위치 지나면 버튼 보이게
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 10) {
+    // 300px 이상 내려갔을 때 보이기
+    scrollTopBtn.classList.add("show");
+  } else {
+    scrollTopBtn.classList.remove("show");
+  }
+});
+
+// 버튼 클릭 시 상단으로 부드럽게 이동
+scrollTopBtn.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});
